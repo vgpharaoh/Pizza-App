561330b97ef3cf26ecb1bc90ad49703e9a20c037
vgpharaoh <60716580+vgpharaoh@users.noreply.github.com>
2022-11-08 21:41:48 -0700

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import setup
+
+setup(name='Pizza-App',
+      version='1.0',
+      description='create pizzas and choose what the chefs can use on the pizzas!',
+      author='Hussein Amr',
+      author_email='husseinamr7@gmail.com',
+      packages=find_packages(),
+     )
